db22cdbdbe48b50c90c239be2f122f234469b955
Abdulrahman Auta <autaa638@gmail.com>
2026-05-13 04:06:20 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from  fastapi import FastAPI
+from fastapi import FastAPI
 from model import Product
 app = FastAPI() 
 @app.get('/')
@@ -6,11 +6,52 @@ def greet():
     return 'welcome to telusko trac'
 
 Products = [
-    Product(1,'phone', 'budget phone',99,10),
-    Product(2, 'laptop','gaming laptop',999,6)
+    Product(id=1,name='phone', description='Smartphone', price=699.99, quantity=50),
+    Product(id=2, name='laptop', description='gaming laptop', price=999.99, quantity=30),
+    Product(id=5, name='pen', description='A blue ink pen',price=1.99,quantity=100),
+    Product(id=6, name='Table',description='A wooden table', price=199.99, quantity=20)
 ]
+@app.get('/products')
+def get_products():
+    return Products
+
+@app.get('/products/{id}') 
+def get_products_by_id(id: int):
+    for product in product:
+        if product.id ==id:
+         return product
+    
+ 
+    return 'product not found' 
+      
 
 
-@app.get('/products') 
-def get_all_products():
-   return products   
+@app.post('/product')
+def add_product(products:Product):
+    Products.append(Product)
+    return Product
+
+@app.put('/product')
+def update_product(id:int, product:Product):
+    for i in range (len(product)):
+        if product[i].id ==id:
+            product[i] = product
+            return 'prodcut Added successfully'   
+
+    return 'no product found'
+
+
+@app.delete('/product')
+def delete_product(id: int):
+    for i in range (len(product)):
+        if product [i].id ==id:
+            del product[i]
+            return 'product Deleted succesfully'
+
+
+            return 'no product found'
+
+
+
+
+
